eeb3529c621c499f160faf4ff74ed3e7d80a5e99
Nathan Longhurst <nathanlonghurst@gmail.com>
2026-05-13 15:24:33 -0600

    updated name

--- setup.sh
+++ setup.sh
@@ -349,7 +349,8 @@ prompt_vault_prefix() {
     default="${VAULT_PREFIX:-API-APP}"
     log_info "Used to name your 1Password vaults: <PREFIX>-LOCAL, <PREFIX>-TEST, <PREFIX>-PROD"
     while :; do
-        val="$(ask_input "Change the vault prefix" "$default")"
+        read -r -p "Change the vault prefix: " val
+        [ -z "$val" ] && val="$default"
         if [[ ! "$val" =~ ^[A-Z0-9-]+$ ]]; then
             log_err "Prefix must contain only uppercase letters, digits, and dashes."
             continue
